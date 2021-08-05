08c29df7d6738cb3da3447aaa198f56f2eba7dba
Joyline254 <joylinechep@kabarak.ac.ke>
2021-08-05 16:22:17 +0300

    style the footer

--- js/script.js
+++ js/script.js
@@ -1,17 +1,6 @@
-(function() {
-    'use strict';
-    window.addEventListener('load', function() {
-      // Get the forms we want to add validation styles to
-      var forms = document.getElementsByClassName('needs-validation');
-      // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('button', function(event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add('was-validated');
-        }, false);
-      });
-    }, false);
-  })();
+$(document).ready(function(){
+    $("#h-six").click(function(){
+        //$("#hide-about-us").show();
+        $("#hide-about-us").toggle();
+    });
+});
