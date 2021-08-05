b4158932bd705ca47d5dbd880dd74f1e4dd6a40b
Joyline254 <joylinechep@kabarak.ac.ke>
2021-08-05 16:40:52 +0300

    Hide and show the email to log in

--- js/script.js
+++ js/script.js
@@ -2,5 +2,12 @@ $(document).ready(function(){
     $("#h-six").click(function(){
         //$("#hide-about-us").show();
         $("#hide-about-us").toggle();
+
+    })
+    $("#main-log-in").click(function(){
+        $("#log-in-form").toggle();
     });
+
 });
+
+
