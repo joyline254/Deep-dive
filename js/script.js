d1d6014543db4af52da8b7ee4d34a97ea15a4538
Samueelx <gsammygitau@gmail.com>
2021-08-05 15:50:29 +0300

    add show functionality

--- js/script.js
+++ js/script.js
@@ -2,4 +2,12 @@ $(document).ready( function() {
     $('.assesment h2').click( function() {
         $('.col-md-3 img').toggle();
     });
+
+    $('.hiring').click( function() {
+        $('.profiles').toggle();
+    });
+    $('.jobs').click( function() {
+        $('#jobs-p').toggle();
+        $('#jobs-a').toggle();
+    });
 });
