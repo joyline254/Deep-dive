d7d55845f94e9749ed22f04a5b1f4995bb9a0ec3
jadeve <susan.jadeve@gmail.com>
2021-08-05 16:34:49 +0300

    updated on the partners section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready( function() {
     $('.assesment h2').click( function() {
-        $('.col-md-3 img').toggle();
+        $('.assesment img').toggle();
     });
 
     $('.hiring').click( function() {
